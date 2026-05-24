903717d66799bb939cf604b77c3264b231ad7cbc
Prassanna1995 <prassannavenkatesh1995@gmail.com>
2026-05-24 20:23:47 +0530

    Enhance OperatorsPratice.js with additional arithmetic and logical operations; add settings.json for editor configuration

--- JavascriptLearning/OperatorsPratice.js
+++ JavascriptLearning/OperatorsPratice.js
@@ -1,4 +1,6 @@
+//Assignment Operators
 let x=10,y=20;
+//Arithmetic Operators
 console.log(x+y);
 console.log(y-x);
 console.log(x*y);
@@ -10,3 +12,16 @@ x++;
 console.log(x);
 --y;
 console.log(y);
+
+x=x+y;
+console.log(x);
+
+console.log(x < y ? "x is less than y" : "x is greater than y");
+
+//Logical Operators
+
+//AND && OR || NOT !
+let a=true,b=false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
