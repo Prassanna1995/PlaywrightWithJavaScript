fffd0851ad8083a2b52645b6f10018eac9b1bfc6
Prassanna1995 <prassannavenkatesh1995@gmail.com>
2026-05-24 13:37:13 +0530

    Add initial HTML and JavaScript files for learning purposes

---
+++ JavascriptLearning/myScript.js
@@ -0,0 +1,9 @@
+/*Var let and const*/
+var x;
+let y;
+const z = 10;
+x = 5;
+y = 15;
+document.write("Value of x: " + x + "<br>");
+document.write("Value of y: " + y + "<br>");
+document.write("Value of z: " + z + "<br>");
