00c20f30c47528ee71bdad8c1302228fbd7f90d9
Prassanna1995 <prassannavenkatesh1995@gmail.com>
2026-05-24 13:44:39 +0530

    Remove commented-out variable declarations and document.write calls; add console.log for variable x

--- JavascriptLearning/myScript.js
+++ JavascriptLearning/myScript.js
@@ -8,10 +8,13 @@
 // document.write("Value of y: " + y + "<br>");
 // document.write("Value of z: " + z + "<br>");
 
-var x="Welcome to JavaScript!", y = 100;
-document.write(x + "<br>");
-document.write("Value of y: " + y + "<br>");
+// var x="Welcome to JavaScript!", y = 100;
+// document.write(x + "<br>");
+// document.write("Value of y: " + y + "<br>");
 
 
 //var is function Scope and let and const are block scope
+console.log(x);
+var x =100;
+console.log(x);
 
