f244d15d1eb09a1d18fdb4ade84b83a99bb48bb1
Prassanna1995 <prassannavenkatesh1995@gmail.com>
2026-05-24 13:41:49 +0530

    Refactor variable declarations and update output messages in myScript.js

--- JavascriptLearning/myScript.js
+++ JavascriptLearning/myScript.js
@@ -1,9 +1,17 @@
 /*Var let and const*/
-var x;
-let y;
-const z = 10;
-x = 5;
-y = 15;
-document.write("Value of x: " + x + "<br>");
+// var x;
+// let y;
+// const z = 10;
+// x = 5;
+// y = 15;
+// document.write("Value of x: " + x + "<br>");
+// document.write("Value of y: " + y + "<br>");
+// document.write("Value of z: " + z + "<br>");
+
+var x="Welcome to JavaScript!", y = 100;
+document.write(x + "<br>");
 document.write("Value of y: " + y + "<br>");
-document.write("Value of z: " + z + "<br>");
+
+
+//var is function Scope and let and const are block scope
+
